99e6bb911fe37517f20a62442a3b615ea273c122
Vinlagrenouille <vincent.navales@etu.u-pec.fr>
2016-06-07 14:46:16 +0200

    added insert into tables of MuAdmin, MuArtist and MuAlbum

---
+++ SQL/tableInsert.sql
@@ -0,0 +1,24 @@
+/* MuAdmin */
+insert into MuAdmin values ("vingren","phpProj2016");
+
+insert into MuArtiste(nameA,birthDate,description) values ("Artic Monkeys","2006-01-01","English rock group");
+insert into MuArtiste(nameA,birthDate,description) values ("IAM","1989-01-01","French rap group");
+insert into MuArtiste(nameA,birthDate,description) values ("NTM","1988-01-01","French rap group");
+insert into MuArtiste(nameA,birthDate,description) values ("Muse","1994-01-01","English rock group");
+insert into MuArtiste(nameA,birthDate,description) values ("Wagner","1813-05-22","German composer");
+insert into MuArtiste(nameA,birthDate,description) values ("Daft Punk","1993-01-01"," French Electronic group");
+insert into MuArtiste(nameA,birthDate,description) values ("Akhenaton","1968-09-17","French Rapper");
+insert into MuArtiste(nameA,birthDate,description) values ("Orelsan","1982-08-01","French rapper"); 
+insert into MuArtiste(nameA,birthDate,description) values ("Bach","1685-03-31","German composer");
+insert into MuArtiste(nameA,birthDate,description) values ("B2O","1976-12-09","French Rapper");
+
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Favourite Worst Nightmare","2007-04-23","rock",1);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Futur","2012-11-26","rap",10);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("D.U.C","2012-04-13","rap",10);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Concertos pour violons","1700-01-01","barock",9);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Showbiz","1999-09-07","rock",4);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Absolution","2003-09-22","rock",4);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("The Resistance","2009-09-14","rock",4);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Drones","2015-06-05","rock",4);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Authentik","1993-03-05","rap",3);
+insert into MuAlbum(name,releaseDate,genre,idArtist) values ("1993","1991-06-03","rap",3);
