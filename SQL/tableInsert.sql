20ca097e4097dc98a6e16889213ac1620eeb0970
vincent.navales <vincent.navales@hotmail.fr>
2016-06-08 03:16:11 -0400

    correct SQL : tableInsert.sql

--- SQL/tableInsert.sql
+++ SQL/tableInsert.sql
@@ -1,16 +1,16 @@
 /* MuAdmin */
 insert into MuAdmin values ("vingren","phpProj2016");
 
-insert into MuArtiste(nameA,birthDate,description) values ("Artic Monkeys","2006-01-01","English rock group");
-insert into MuArtiste(nameA,birthDate,description) values ("IAM","1989-01-01","French rap group");
-insert into MuArtiste(nameA,birthDate,description) values ("NTM","1988-01-01","French rap group");
-insert into MuArtiste(nameA,birthDate,description) values ("Muse","1994-01-01","English rock group");
-insert into MuArtiste(nameA,birthDate,description) values ("Wagner","1813-05-22","German composer");
-insert into MuArtiste(nameA,birthDate,description) values ("Daft Punk","1993-01-01"," French Electronic group");
-insert into MuArtiste(nameA,birthDate,description) values ("Akhenaton","1968-09-17","French Rapper");
-insert into MuArtiste(nameA,birthDate,description) values ("Orelsan","1982-08-01","French rapper"); 
-insert into MuArtiste(nameA,birthDate,description) values ("Bach","1685-03-31","German composer");
-insert into MuArtiste(nameA,birthDate,description) values ("B2O","1976-12-09","French Rapper");
+insert into MuArtist(nameA,birthDate,description) values ("Artic Monkeys","2006-01-01","English rock group");
+insert into MuArtist(nameA,birthDate,description) values ("IAM","1989-01-01","French rap group");
+insert into MuArtist(nameA,birthDate,description) values ("NTM","1988-01-01","French rap group");
+insert into MuArtist(nameA,birthDate,description) values ("Muse","1994-01-01","English rock group");
+insert into MuArtist(nameA,birthDate,description) values ("Wagner","1813-05-22","German composer");
+insert into MuArtist(nameA,birthDate,description) values ("Daft Punk","1993-01-01"," French Electronic group");
+insert into MuArtist(nameA,birthDate,description) values ("Akhenaton","1968-09-17","French Rapper");
+insert into MuArtist(nameA,birthDate,description) values ("Orelsan","1982-08-01","French rapper"); 
+insert into MuArtist(nameA,birthDate,description) values ("Bach","1685-03-31","German composer");
+insert into MuArtist(nameA,birthDate,description) values ("B2O","1976-12-09","French Rapper");
 
 insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Favourite Worst Nightmare","2007-04-23","rock",1);
 insert into MuAlbum(name,releaseDate,genre,idArtist) values ("Futur","2012-11-26","rap",10);
