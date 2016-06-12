3e0ff29dc21f5ba435d5c2674243c54a1ed18d09
vincent.navales <vincent.navales@hotmail.fr>
2016-06-12 09:00:05 -0400

    edit header showArtist

--- soundlink/application/views/Artists/showArtist.php
+++ soundlink/application/views/Artists/showArtist.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8"/>
-		<link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet" />
-		<title>Artist</title>
-	</head>
-	<body>
-		<div class="container">
-			<div class="page-header">
-				<ul class="nav nav-pills pull-right">
-					<li> <a href="<?php echo base_url().'albums';?>">List of albums</a></li>
-					<li> <a href="">About</a></li>
-				</ul>
-				<h3 class="muted">Soundlink
+<head>
+<meta charset="utf-8" />
+<link href="<?php echo base_url(); ?>assets/css/bootstrap.css"
+	rel="stylesheet" />
+<title>Artists</title>
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+			<ul class="nav nav-pills pull-right">
+				<li><a href="<?php echo base_url().'AlbumController';?>">Albums</a></li>
+				<li><a href="<?php echo base_url().'ArtistController';?>">Artists</a></li>
+				<li><a href="<?php echo base_url().'ProfileController';?>">My profile</a></li>
+				<li><a href="<?php echo base_url().'AboutConntroller';?>">About</a></li>
+			</ul>
+			<h3>
+				<a href="<?php echo base_url();?>">Soundlink</a>
 			</h3>
 		</div>
       <?php
