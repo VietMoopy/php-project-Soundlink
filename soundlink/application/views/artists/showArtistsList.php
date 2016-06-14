35041b41653efc4099e459ff4b8a2c1567b55271
VietMoopy <alexandrenguyen68817@gmail.com>
2016-06-14 22:31:23 +0200

    Changed showArtistsList View to make it work with searching

--- soundlink/application/views/artists/showArtistsList.php
+++ soundlink/application/views/artists/showArtistsList.php
@@ -12,7 +12,7 @@
 			<ul class="nav nav-pills pull-right">
 				<li><a href="<?php echo base_url().'AlbumController';?>">Albums</a></li>
 				<li><a href="<?php echo base_url().'ArtistController';?>">Artists</a></li>
-				<li><a href="<?php echo base_url().'InscriptionController';?>">Inscription</a></li>
+					<li><a href="<?php echo base_url().'InscriptionController';?>">Inscription</a></li>
 				<li><a href="<?php echo base_url().'AboutController';?>">About</a></li>
 			</ul>
 			<h3>
@@ -20,10 +20,10 @@
 			</h3>
 		</div>
 		<p class="lead">Artists List</p>
-		<a href="<?php echo base_url().'ArtistController/addArtist';?>">Add an artist</a>
-		<br/><br/>
-		<a href="<?php echo base_url().'ResearchController/searchArtist';?>">Find an artist</a>
-		<br/><br/>
+		<a href="<?php echo base_url().'ArtistController/addArtist';?>">Add an
+			artist</a> <br /> <br /> <a
+			href="<?php echo base_url().'ResearchController/searchArtist';?>">Find
+			an artist</a> <br /> <br />
 		<table>
 			<thead>
 				<tr>
@@ -31,15 +31,20 @@
 				</tr>
 			</thead>
 		  <?php
-				if(!isset($name)){
-					$name = null;
-				}
-				//$query = $this->MuArtist->get_per($name);
-				$query = $this->MuArtist->get_all();
-				foreach($query as $row) {
-					echo "<tr>";
-					echo "<td><a href='".base_url().'ArtistController/showArtist/'.$row->idArtist.'\'>'.$row->nameA."</a></td>";
-					echo "</tr>";
+				if ($name != '') {
+					$query = $this->MuArtist->get_from_name ( $name );
+					foreach ( $query as $row ) {
+						echo "<tr>";
+						echo "<td><a href='" . base_url () . 'ArtistController/showArtist/' . $row->idArtist . '\'>' . $row->nameA . "</a></td>";
+						echo "</tr>";
+					}
+				} else {
+					$query = $this->MuArtist->get_all ();
+					foreach ( $query as $row ) {
+						echo "<tr>";
+						echo "<td><a href='" . base_url () . 'ArtistController/showArtist/' . $row->idArtist . '\'>' . $row->nameA . "</a></td>";
+						echo "</tr>";
+					}
 				}
 				?>
 		</table>
