16e78048789bbf3d96480c1de668423374067b5c
vincent.navales <vincent.navales@hotmail.fr>
2016-06-14 16:06:42 -0400

    inscription done

--- soundlink/application/views/artists/addArtist.php
+++ soundlink/application/views/artists/addArtist.php
@@ -12,8 +12,7 @@
 			<ul class="nav nav-pills pull-right">
 				<li><a href="<?php echo base_url().'AlbumController';?>">Albums</a></li>
 				<li><a href="<?php echo base_url().'ArtistController';?>">Artists</a></li>
-				<li><a href="<?php echo base_url().'ProfileController';?>">My
-						profile</a></li>
+				<li><a href="<?php echo base_url().'InscriptionController';?>">Inscription</a></li>
 				<li><a href="<?php echo base_url().'AboutConntroller';?>">About</a></li>
 			</ul>
 			<h3>
