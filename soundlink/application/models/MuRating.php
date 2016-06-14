45e8752c52725553106bea4044808b926f221eed
VietMoopy <alexandrenguyen68817@gmail.com>
2016-06-15 00:29:37 +0200

    Adapted update and insert function, Added checkRating function

--- soundlink/application/models/MuRating.php
+++ soundlink/application/models/MuRating.php
@@ -1,43 +1,51 @@
 <?php
-
 class MuRating extends CI_Model {
-
-    public $idRating;
-    public $ratingvalue;
-    public $dateRating;
-    public $username;
-    public $idAlbum;
-
-    public function __construct() {
-        parent::__construct();
-    }
-
-    public function get($id){
-        return $this->db->get_where('MuRating', array('id' => $id))->row();
-    }
-
-    public function get_all() {
-        $query = $this->db->get('MuRating');
-        return $query->result();
-    }
-
-    public function insert($value,$album) {
-    	$fielddata['ratingValue'] = $value;
-    	$fielddata['dateRating'] = date();
-    	$fielddata['username'] = 'not yet';
-    	$fielddata['idAlbum'] = $album;
-      $this->db->insert('MuRating', $fielddata);
-    }
-
-    public function update($id) {
-        $this->title = 'CodeIgniter 101';
-        $this->content = '<p>Say what you want about CI, it still rocks</p>';
-        $this->date = time();
-
-        $this->db->update('MuRating', $this, array('id' => $id));
-    }
-
-    public function delete($id){
-        $this->db->delete('MuRating', array('id' => $id)); 
-    }
+	public $idRating;
+	public $ratingvalue;
+	public $dateRating;
+	public $username;
+	public $idAlbum;
+	public function __construct() {
+		parent::__construct ();
+	}
+	public function get($id) {
+		return $this->db->get_where ( 'MuRating', array (
+				'id' => $id 
+		) )->row ();
+	}
+	public function get_all() {
+		$query = $this->db->get ( 'MuRating' );
+		return $query->result ();
+	}
+	public function insert($value, $user, $idAlbum) {
+		$fielddata ['ratingValue'] = $value;
+		$datestring = '%Y-%m-%d';
+		$fielddata ['dateRating'] = mdate ( $datestring );
+		$fielddata ['username'] = $user;
+		$fielddata ['idAlbum'] = $idAlbum;
+		$this->db->insert ( 'MuRating', $fielddata );
+	}
+	public function checkRating($idAlbum, $user) {
+		$this->db->select ( '*' );
+		$this->db->where ( 'idAlbum', $idAlbum );
+		$this->db->where ( 'username', $user );
+		$this->db->from ( 'MuRating' );
+		$query = $this->db->get ()->row();
+		if ($query != NULL)
+			return $query;
+		else
+			return null;
+	}
+	public function update($value,$id) {
+		$fielddata ['ratingValue'] = $value;
+		$datestring = '%Y-%m-%d';
+		$fielddata ['dateRating'] = mdate ( $datestring );
+		$this->db->where('id', $id);
+		$this->db->update('MuRating', $fielddata);
+	}
+	public function delete($id) {
+		$this->db->delete ( 'MuRating', array (
+				'id' => $id 
+		) );
+	}
 }
